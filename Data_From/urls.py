55c79fd82c5d26efe76b77d6cb084f6929e20ff3
anirbanpal2002 <anirbanpal022@gmail.com>
2025-01-05 19:16:45 +0530

    signin

--- Data_From/urls.py
+++ Data_From/urls.py
@@ -4,6 +4,9 @@ from .import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    path('',views.index, name='index'),
     path('home',views.home, name='home'),
-    path('product_list',views.product_list, name='product_list'),
+    path('sign',views.sign, name='sign'),
+    path('cong',views.cong, name='cong'),
+    # path('product_list',views.product_list, name='product_list'),
 ]
--- Data_From/views.py
+++ Data_From/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import User
 
@@ -10,13 +10,48 @@ def home(request):
         email = request.POST['email']
         phone = request.POST['phone']
         password = request.POST['password']
-        user = User(name=name,email=email,phone=phone,password=password)
-        user.save()
-        return HttpResponse('Data Saved Successfully')
+        cpassword = request.POST['cpassword']
+        if password==cpassword:
+            user = User(name=name,email=email,phone=phone,password=password)
+            user.save()
+            return redirect('cong')
+        else:
+            return HttpResponse('Password not matched')
     return render(request,'Data_From/login.html')
 
-def product_list(request):
-    # Fetch all products
-    datas = User.objects.all()
-    print(datas)
-    return render(request, 'Data_from/datafetch.html', {'datas': datas})
+
+def sign(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        pass1= request.POST['password']
+        user=authentication(email,pass1)
+        if user:
+            return redirect('cong')
+        else:
+            return HttpResponse('Invalid credentials')
+    
+        
+    return render(request,'Data_From/sign.html')
+
+
+
+def authentication(email,pass1):
+    try:
+        user = User.objects.get(email=email,password=pass1)
+        return user
+    except User.DoesNotExist:  
+        return None
+    
+def index(request):
+    return render(request,'Data_From/home.html')
+
+def cong(request):
+    return render(request,'Data_From/cong.html')
+
+# def product_list(request):
+#     # Fetch all products
+#     datas = User.objects.all()
+#     print(datas)
+#     return render(request, 'Data_from/datafetch.html', {'datas': datas})
+
+
